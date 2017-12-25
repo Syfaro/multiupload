f91e9af8bb586b367cf58b2e12c1dd341c7af1ca
Syfaro <syfaro@huefox.com>
2017-12-25 15:58:33 -0600

    Update db.sql file.

--- db.sql
+++ db.sql
@@ -3,4 +3,6 @@ INSERT INTO `site` (`id`, `name`) VALUES
     (2, 'Weasyl'),
     (3, 'FurryNetwork'),
     (4, 'Inkbunny'),
-    (5, 'SoFurry');
+    (5, 'SoFurry'),
+    (7, 'Tumblr'),
+    (100, 'Twitter');
