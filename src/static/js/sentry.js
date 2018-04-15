0078a35dbeeae864fe320e044be7cf6872c166b4
Syfaro <syfaro@huefox.com>
2018-04-15 15:31:35 -0500

    Move user to JS object, tojson for security.

--- src/static/js/sentry.js
+++ src/static/js/sentry.js
@@ -1,18 +1,14 @@
-Raven.config(Multiupload.sentry).install();
+if (Multiupload.sentry) {
+    Raven.config(Multiupload.sentry).install();
 
-if (Multiupload.release) {
-    Raven.setRelease(Multiupload.release);
+    if (Multiupload.release) {
+        Raven.setRelease(Multiupload.release);
 
-    window.addEventListener('load', () => {
-        document.querySelector('.revision').classList.remove('d-none');
-        document.querySelector('.revision a').style.color = '#' + Multiupload.release.slice(-6);
-    });
+        window.addEventListener('load', () => {
+            document.querySelector('.revision').classList.remove('d-none');
+            document.querySelector('.revision a').style.color = '#' + Multiupload.release.slice(-6);
+        });
+    }
+
+    if (Multiupload.user) Raven.setUserContext(Multiupload.user);
 }
-
-const userID = document.querySelector('meta[name="user-id"]');
-const userEmail = document.querySelector('meta[name="user-email"]');
-
-Raven.setUserContext({
-    email: userEmail ? userEmail.content : null,
-    id: userID ? userID.content : null,
-});
