8d4e0db51a19caf41dfabc16a6e98d0387b2e29f
Syfaro <syfaro@huefox.com>
2018-04-21 01:27:11 -0500

    Preview image from list on hover.

---
+++ src/static/js/list/preview.js
@@ -0,0 +1,18 @@
+class ImagePreview {
+    constructor() {
+        this.submissions = Array.from(document.querySelectorAll('tbody tr th'));
+        this.submissions.forEach(submission => {
+            $(submission).popover({
+                content: function () {
+                    const parent = this.parentNode;
+                    const imageSrc = parent.dataset.image;
+                    return `<img style="max-width: 200px; max-height: 200px;" src="/upload/imagepreview/${imageSrc}">`;
+                },
+                trigger: 'hover',
+                html: true,
+            });
+        });
+    }
+}
+new ImagePreview();
+//# sourceMappingURL=preview.js.map
---
+++ src/static/ts/list/preview.ts
@@ -0,0 +1,20 @@
+class ImagePreview {
+    private submissions = Array.from(document.querySelectorAll('tbody tr th')) as HTMLTableHeaderCellElement[];
+
+    constructor() {
+        this.submissions.forEach(submission => {
+            $(submission).popover({
+                content: function () {
+                    const parent = this.parentNode;
+                    const imageSrc = parent.dataset.image;
+
+                    return `<img style="max-width: 200px; max-height: 200px;" src="/upload/imagepreview/${imageSrc}">`;
+                },
+                trigger: 'hover',
+                html: true,
+            })
+        });
+    }
+}
+
+new ImagePreview();
