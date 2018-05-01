ff08fa682f595f812f495588db7004786dc991d7
Syfaro <syfaro@huefox.com>
2018-05-01 01:33:55 -0500

    Mascot(!) and improve features page.

--- src/static/js/sentry.js
+++ src/static/js/sentry.js
@@ -1,14 +1,16 @@
-if (Multiupload.sentry) {
-    Raven.config(Multiupload.sentry).install();
-
-    if (Multiupload.release) {
-        Raven.setRelease(Multiupload.release);
-
-        window.addEventListener('load', () => {
+if (Multiupload.release) {
+     window.addEventListener('load', () => {
             document.querySelector('.revision').classList.remove('d-none');
             document.querySelector('.revision a').style.color = '#' + Multiupload.release.slice(-6);
         });
     }
 
+if (Multiupload.sentry) {
+    Raven.config(Multiupload.sentry).install();
+
     if (Multiupload.user) Raven.setUserContext(Multiupload.user);
+}
+
+if (Multiupload.release && Multiupload.sentry) {
+    Raven.setRelease(Multiupload.release);
 }
